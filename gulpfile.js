531f6a1496824358c49c598ad5c0baa7d47e6a13
liberat0r <lib3rat0r@gmail.com>
2015-04-07 01:01:26 +0300

    step 2 init

--- gulpfile.js
+++ gulpfile.js
@@ -15,11 +15,21 @@ var scss = require('gulp-sass'); // Gulp libsass implementation
 /**
  * Compiles SCSS files into CSS
  *
- * @source .scss files
+ * @src .scss files that do not start with _
  * @dest .css files
  */
 gulp.task('scss', function() {
-    return gulp.src('scss/*.scss')
+    return gulp.src(['scss/**/*.scss', '!scss/**/_*'])
         .pipe(scss())
         .pipe(gulp.dest('css'));
+});
+
+/**
+ * The default task that is run with the
+ * gulp command when not task is specified.
+ * The scss task is required to run before
+ * this one.
+ */
+gulp.task('default', ['scss'], function() {
+    gulp.watch('scss/**/*.scss', ['scss']);
 });
